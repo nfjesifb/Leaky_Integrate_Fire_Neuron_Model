3560b84ada743c111e75afbec192c650411bb35d
nfjesifb <mvsegal08@gmail.com>
2023-11-02 00:02:17 -0700

    fixes?

--- src/subtractor.v
+++ src/subtractor.v
@@ -1,6 +1,6 @@
 module subtractor( input [7:0] V, output reg [7:0] V_prime);
     always @* begin
-        if(V) begin
+        if(V != 8'b00000000) begin
             V_prime = V - 1;
         end
         else begin
